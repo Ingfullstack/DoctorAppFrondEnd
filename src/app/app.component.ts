9077f35219ad91a6f14c3b924f293ac92bdd4fa6
Carlos Ayala <ing.full.stack@hotmail.com>
2024-03-06 20:48:50 -0400

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'DoctorAppFrondEnd';
+export class AppComponent implements OnInit {
+
+  public usuarios:any;
+
+  public title:string = 'Bienvenido a DoctorApp';
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.http.get('http://localhost:5267/api/usuarios')
+        .subscribe({
+          next: response => this.usuarios = response,
+          complete: () => console.log("Completado"),
+          error: err => console.log(err)
+        });
+  }
 }
